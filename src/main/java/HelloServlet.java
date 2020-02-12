557cb6e2b4dbf669100aceee7f04b00250f07632
Nose <614792109@qq.com>
2020-02-12 16:25:50 +0800

    test

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Date;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().println("<h1>daadasd</h1>");
+        response.getWriter().println(new Date().toString());
+    }
+}
