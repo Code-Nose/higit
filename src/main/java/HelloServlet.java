2a2ab6fea3487dfd19116162b12d1da59563afbe
Nose <614792109@qq.com>
2020-02-12 16:35:59 +0800

    test

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
 
     @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().println("<h1>dd大麻申川d</h1>");
+        response.getWriter().println("<h1>dd大麻1231231d</h1>");
         response.getWriter().println(new Date().toString());
     }
 }
